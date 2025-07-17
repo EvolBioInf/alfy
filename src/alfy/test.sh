2153435f3efa90a6d507c619259c1c90688f8ed0
Bernhard Haubold <haubold@evolbio.mpg.de>
2025-07-17 12:24:45 +0200

    Deleted C sources

--- src/alfy/test.sh
+++
@@ -1,8 +0,0 @@
-./alfy -i ../../data/A+DQ083238.fasta -j ../../data/hiv42.fasta > test.txt
-d=$(diff test.txt r.txt)
-if [[ d -ne "" ]]; then
-    echo "alfy: failed"
-else
-    echo "alfy: passed"
-fi
-rm test.txt
