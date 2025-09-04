f5af2f68f4c8b09f0fc66286bed300337e0e6c1e
Beatriz Mourato <mourato@evolbio.mpg.de>
2025-09-04 18:51:40 +0200

    Skeleton of alfy

--- alfy/alfy.go
+++ alfy/alfy.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"flag"
 	"github.com/evolbioinf/alfy/util"
+	"github.com/evolbioinf/clio"
 )
 
 func main() {
-	util.Version("alfy")
+	util.PrepareErrorMessages("alfy")
+	optV := flag.Bool("v", false, "version")
+	u := "alfy <prepAlfy.out>"
+	p := "Calculate homology between queries and subjects"
+	e := "alfy alfy.in"
+	clio.Usage(u, p, e)
+	flag.Parse()
+	if *optV {
+		util.Version("alfy")
+	}
+
 }
