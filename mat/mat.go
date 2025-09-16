96435b3b7af584c022d02cfa16dceab36501958b
Beatriz Mourato <mourato@evolbio.mpg.de>
2025-09-16 13:21:28 +0200

    mat package now returns a slice of subject IDs whenever the stored match is found across multiple subjects.

--- mat/mat.go
+++ mat/mat.go
@@ -3,11 +3,12 @@ package mat
 
 import (
 	"github.com/evolbioinf/esa"
+	"slices"
 )
 
-// The function UpdateMatchLengths takes as arguments a query sequence, the enhanced suffix array (ESA) of a subject sequence, the label of that subject sequence, an array of match lengths, an array of subject labels, and whether or not the query is a reverse strand. It then updates the arrays of match lengths and subject labels.
+// The function UpdateMatchLengths takes as arguments a query sequence, the enhanced suffix array (ESA) of a subject sequence, the label of that subject sequence, an array of match lengths, a double array of subject labels, and whether or not the query is a reverse strand. It then updates the arrays of match lengths and subject labels.
 func UpdateMatchLengths(q []byte, s *esa.Esa, i int,
-	ml, su []int, rev bool) {
+	ml []int, su [][]int, rev bool) {
 	j := 0
 	m := len(q)
 	for j < m {
@@ -22,15 +23,30 @@ func UpdateMatchLengths(q []byte, s *esa.Esa, i int,
 		}
 		if l > ml[p] {
 			ml[p] = l
-			su[p] = i
+			su[p][0] = i
+		} else if l == ml[p] {
+			su[p] = append(su[p], i)
 		}
 		j += l + 1
 	}
 	for i := 1; i < len(q); i++ {
 		x := ml[i-1] - 1
-		if x >= ml[i] {
+		if x < 0 {
+			x = 0
+		}
+		if x > ml[i] {
 			ml[i] = x
-			su[i] = su[i-1]
+			su[i][0] = su[i-1][0]
+		} else if x == ml[i] {
+			c := 0
+			for z := 0; z < len(su[i]); z++ {
+				if !(slices.Contains(su[i-1], su[i][z])) {
+					su[i][c] = su[i][z]
+					c++
+				}
+			}
+			su[i] = su[i][:c]
+			su[i] = append(su[i], su[i-1]...)
 		}
 	}
 }
--- mat/mat_test.go
+++ mat/mat_test.go
@@ -2,13 +2,28 @@ package mat
 
 import (
 	"github.com/evolbioinf/esa"
+	"slices"
 	"testing"
 )
 
-func check(t *testing.T, get, want []int) {
+func checkM(t *testing.T, get, want []int) {
 	diff := false
 	for i, _ := range get {
 		if get[i] != want[i] {
+			diff = true
+			break
+		}
+	}
+	if diff {
+		t.Errorf("\nget:  %v\n"+
+			"want: %v",
+			get, want)
+	}
+}
+func checkS(t *testing.T, get, want [][]int) {
+	diff := false
+	for i, _ := range get {
+		if slices.Compare(get[i], want[i]) != 0 {
 			diff = true
 			break
 		}
@@ -29,24 +44,24 @@ func TestMat(t *testing.T) {
 	wml := [][]int{
 		[]int{4, 3, 2, 5, 4, 3, 2, 1, 0, 1},
 		[]int{4, 3, 2, 2, 1, 0, 4, 3, 2, 1}}
-	wsu := [][]int{
-		[]int{0, 0, 0, 1, 1, 1, 1, 1, 1, 0},
-		[]int{0, 0, 0, 1, 1, 1, 1, 1, 1, 1}}
+	wsu := [][][]int{
+		[][]int{{0}, {0}, {0}, {1}, {1}, {1}, {1}, {1}, {0, 1}, {0, 1}},
+		[][]int{{0}, {0}, {0}, {1}, {1}, {0, 1}, {1}, {1}, {1}, {0, 1}}}
 	rev := false
 	for i, q := range queries {
 		n := len(q)
 		ml := make([]int, n)
-		su := make([]int, n)
+		su := make([][]int, n)
 		for j := 0; j < n; j++ {
 			ml[j] = -1
-			su[j] = -1
+			su[j] = append(su[j], -1)
 		}
 		for j, subject := range subjects {
 			s := esa.MakeEsa(subject)
 			UpdateMatchLengths(q, s, j, ml, su, rev)
 		}
-		check(t, ml, wml[i])
-		check(t, su, wsu[i])
+		checkM(t, ml, wml[i])
+		checkS(t, su, wsu[i])
 	}
 	queries = [][]byte{
 		[]byte("ACTATTATTT"),
@@ -54,23 +69,23 @@ func TestMat(t *testing.T) {
 	wml = [][]int{
 		[]int{2, 2, 1, 2, 4, 3, 2, 1, 0, 1},
 		[]int{2, 4, 3, 2, 1, 0, 4, 3, 2, 1}}
-	wsu = [][]int{
-		[]int{0, 1, 1, 0, 1, 1, 1, 1, 1, 0},
-		[]int{0, 1, 1, 1, 1, 1, 1, 1, 1, 1}}
+	wsu = [][][]int{
+		[][]int{{0}, {1}, {1}, {0}, {1}, {1}, {0, 1}, {0, 1}, {0, 1}, {0, 1}},
+		[][]int{{0}, {1}, {1}, {0, 1}, {0, 1}, {0, 1}, {1}, {0, 1}, {0, 1}, {0, 1}}}
 	rev = true
 	for i, q := range queries {
 		n := len(q)
 		ml := make([]int, n)
-		su := make([]int, n)
+		su := make([][]int, n)
 		for j := 0; j < n; j++ {
 			ml[j] = -1
-			su[j] = -1
+			su[j] = append(su[j], -1)
 		}
 		for j, subject := range subjects {
 			s := esa.MakeEsa(subject)
 			UpdateMatchLengths(q, s, j, ml, su, rev)
 		}
-		check(t, ml, wml[i])
-		check(t, su, wsu[i])
+		checkM(t, ml, wml[i])
+		checkS(t, su, wsu[i])
 	}
 }
