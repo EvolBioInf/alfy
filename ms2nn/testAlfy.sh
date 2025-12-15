ae6daa3a0bfd88885c226c2afcc2dc51e4e02969
Bernhard Haubold <haubold@evolbio.mpg.de>
2025-12-15 08:39:56 +0100

    Cleaning codebase for ms2nn

--- ms2nn/testAlfy.sh
+++ ms2nn/testAlfy.sh
@@ -1,16 +1,11 @@
-# Parameters
 n=5
 theta=100
 rho=1
 len=10000
-# Generate haplotypes
 ms $n 1 -t $theta -r $rho $len -T > ms.out
-# Analyze haplotypes with ms2nn
 ms2nn -q 1 ms.out > ms.nn
-# Analyze haplotypes with alfy
 ms2dna ms.out | tr -d S > ms.fasta
 getSeq 1 ms.fasta > q.fasta
 getSeq -c 1 ms.fasta > s.fasta
 alfy -M -i q.fasta -j s.fasta > ms.alfy
-# Compare alfy to truth
 paste ms.alfy ms.nn
