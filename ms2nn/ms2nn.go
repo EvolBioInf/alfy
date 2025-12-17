8d2e630ea2938416bfdc13a2cb8e186e4e7752f8
Bernhard Haubold <haubold@evolbio.mpg.de>
2025-12-17 11:39:38 +0100

    Edited ms2nn

--- ms2nn/ms2nn.go
+++ ms2nn/ms2nn.go
@@ -19,6 +19,7 @@ type Tree struct {
 	start, end int
 	si, ei     int
 	neighbors  []string
+	mm         int
 	query      string
 	root       *nwk.Node
 }
@@ -32,15 +33,16 @@ func parse(r io.Reader, args ...interface{}) {
 	trees := []*Tree{}
 	positions := []float64{}
 	haplotypes := []string{}
-	start := 1
+	start := 0
 	sc := bufio.NewScanner(r)
 	for sc.Scan() {
 		line := sc.Text()
 		if len(line) == 0 {
 			continue
 		}
-		c := line[0]
-		if c == 'm' {
+		fc := line[0]
+		lc := line[len(line)-1]
+		if fc == 'm' {
 			if strings.Index(line, "-T") < 0 {
 				log.Fatal("please use tree printing, -T")
 			}
@@ -49,14 +51,22 @@ func parse(r io.Reader, args ...interface{}) {
 			util.Check(err)
 			for i := 3; i < len(fields); i++ {
 				if fields[i] == "-r" {
-					seqLen, err = strconv.Atoi(fields[i+2])
+					seqLen, err =
+						strconv.Atoi(fields[i+2])
 					util.Check(err)
 				}
 			}
-		} else if c == '[' {
+		} else if lc == ';' {
 			tree := new(Tree)
 			arr := strings.Split(line, "]")
-			ts := arr[1]
+			hasExt := false
+			if len(arr) == 2 {
+				hasExt = true
+			}
+			ts := arr[0]
+			if hasExt {
+				ts = arr[1]
+			}
 			ext, e := strconv.Atoi(arr[0][1:])
 			util.Check(e)
 			tree.start = start
@@ -68,14 +78,14 @@ func parse(r io.Reader, args ...interface{}) {
 			tree.root = sc.Tree()
 			start = tree.end + 1
 			trees = append(trees, tree)
-		} else if c == 'p' {
+		} else if fc == 'p' {
 			fields := strings.Fields(line)
 			for i := 1; i < len(fields); i++ {
 				p, e := strconv.ParseFloat(fields[i], 64)
 				util.Check(e)
 				positions = append(positions, p)
 			}
-		} else if c == '1' || c == '0' {
+		} else if fc == '1' || fc == '0' {
 			haplotypes = append(haplotypes, line)
 		}
 	}
@@ -124,6 +134,7 @@ func parse(r io.Reader, args ...interface{}) {
 					min = dist[i]
 				}
 			}
+			tree.mm = min
 			for i := 0; i < nsam; i++ {
 				if i+1 != qi && dist[i] == min {
 					nei := strconv.Itoa(i + 1)
@@ -135,20 +146,29 @@ func parse(r io.Reader, args ...interface{}) {
 	for _, tree := range trees {
 		sort.Strings(tree.neighbors)
 	}
-	c := 0
+	c := 1
 	for i := 1; i < len(trees); i++ {
-		if slices.Equal(trees[c].neighbors, trees[i].neighbors) {
+		if slices.Equal(trees[c].neighbors,
+			trees[i].neighbors) {
 			trees[c].end = trees[i].end
+			trees[c].mm += trees[i].mm
 		} else {
-			c++
 			trees[c] = trees[i]
+			c++
 		}
 	}
-	trees = trees[0 : c+1]
+	trees = trees[0:c]
+	mm := 0.0
 	fmt.Printf(">%s\n", query)
 	for _, tree := range trees {
-		fmt.Printf("%d\t%d\t-1\t%s",
-			tree.start, tree.end, tree.neighbors[0])
+		mm = -1
+		if !treeAnalysis {
+			l := tree.end - tree.start
+			mm = float64(tree.mm) / float64(l)
+		}
+		fmt.Printf("%d\t%d\t%.4f\t%s",
+			tree.start+1, tree.end+1, mm,
+			tree.neighbors[0])
 		for _, neighbor := range tree.neighbors[1:] {
 			fmt.Printf(" %s", neighbor)
 		}
@@ -168,7 +188,8 @@ func findParent(v *nwk.Node, tree *Tree) {
 func addNeighbors(v *nwk.Node, tree *Tree) {
 	if v != nil {
 		if v.Child == nil && v.Label != tree.query {
-			tree.neighbors = append(tree.neighbors, v.Label)
+			tree.neighbors = append(tree.neighbors,
+				v.Label)
 		}
 		addNeighbors(v.Child, tree)
 		addNeighbors(v.Sib, tree)
@@ -182,7 +203,8 @@ func main() {
 	clio.Usage(u, p, e)
 	flagV := flag.Bool("v", false, "version")
 	flagQ := flag.String("q", "", "query")
-	flagT := flag.Bool("t", false, "tree analysis (default distance analysis)")
+	flagT := flag.Bool("t", false, "tree analysis "+
+		"(default distance analysis)")
 	flag.Parse()
 	if *flagV {
 		util.Version("ms2nn")
