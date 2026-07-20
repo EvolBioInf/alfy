d0ef3f0c1ca440b50cf940f2e86d091a57de5980
Bernhard Haubold <haubold@evolbio.mpg.de>
2026-07-20 13:02:06 +0200

    Improved cross.sh
    - Clarified the meaning of the two mutation rates
    
    - Allow both mutation rates to be set
    
    - Crossover point is now in the middle of the query rather that at
    5000
    
    - Updated the interface of cross in the tutorial

--- cross/cross.sh
+++ cross/cross.sh
@@ -1,38 +1,62 @@
 l=10000
+m=0.1
+M=0.01
 s=$RANDOM
-while getopts "hl:s:" opt; do
+while getopts "hl:m:M:s:" opt; do
     case $opt in
-          l) l=$OPTARG;;
-          s) s=$OPTARG;;
-          h)
-              printf "Usage: bash cross.sh [-h] [option]...\n"
-              printf "Simulate single midpoint crossover between one\n"
-              printf "   query and two subjects in q.fasta and s.fasta.\n"
-              printf "Example: bash cross.sh -s 3\n"
-              printf "   -l int\n\tsequence length (default $l)\n"
-              printf "   -s int\n\tseed (default random integer)\n"
-              exit 0;;
-          ?)
-              printf "Usage: bash cross.sh [-h] [option]...\n"
-              printf "Simulate single midpoint crossover between one\n"
-              printf "   query and two subjects in q.fasta and s.fasta.\n"
-              printf "Example: bash cross.sh -s 3\n"
-              printf "   -l int\n\tsequence length (default $l)\n"
-              printf "   -s int\n\tseed (default random integer)\n"
-              exit 1;;
+        l) l=$OPTARG;;
+        m) m=$OPTARG;;
+        M) M=$OPTARG;;
+        s) s=$OPTARG;;
+        h)
+            echo "Usage: bash cross.sh [-h] [option]..."
+            echo "Simulate single midpoint crossover between"
+            msg="two subjects in s.fasta and one query in q.fasta."
+            echo "   $msg"
+            echo "Example: bash cross.sh -s 3"
+            msg="-l int\n\tsequence length (default $l)\n"
+            printf "   $msg"
+            msg="-m float\n\tmutation rate before crossover"
+            msg="$msg (default $m)\n"
+            printf "   $msg"
+            msg="-M float\n\tmutation rate after crossover"
+            msg="$msg (default $M)\n"
+            printf "   $msg"
+            msg="-s int\n\tseed (default random integer)\n"
+            printf "   $msg"
+            exit 0;;
+        ?)
+        echo "Usage: bash cross.sh [-h] [option]..."
+        echo "Simulate single midpoint crossover between"
+        msg="two subjects in s.fasta and one query in q.fasta."
+        echo "   $msg"
+        echo "Example: bash cross.sh -s 3"
+        msg="-l int\n\tsequence length (default $l)\n"
+        printf "   $msg"
+        msg="-m float\n\tmutation rate before crossover"
+        msg="$msg (default $m)\n"
+        printf "   $msg"
+        msg="-M float\n\tmutation rate after crossover"
+        msg="$msg (default $M)\n"
+        printf "   $msg"
+        msg="-s int\n\tseed (default random integer)\n"
+        printf "   $msg"
+        exit 1;;
     esac
 done
 ranseq -l $l -s $s |
     sed 's/>.*/>s_1/' > s1.fasta
 s=$(($s + 1))
-mutator -m 0.1 -s $s s1.fasta |
+mutator -m $m -s $s s1.fasta |
     sed 's/>.*/>s_2/' > s2.fasta
+mid=$(($l / 2))
 s=$(($s + 1))
-cutSeq -r 1-5000 s1.fasta |
-    mutator -m 0.01 -s $s |
+cutSeq -r 1-$mid s1.fasta |
+    mutator -m $M -s $s |
     sed 's/>.*/>q/' > q.fasta
 s=$((s + 1))
-cutSeq -r 5001-10000 s2.fasta |
-    mutator -m 0.01 -s $s |
+cutSeq -r $(($mid + 1))-$l s2.fasta |
+    mutator -m $M -s $s |
     tail -n +2 >> q.fasta
 cat s[12].fasta > s.fasta
+rm s[12].fasta
